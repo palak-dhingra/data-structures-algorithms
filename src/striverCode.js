fdb8e51b7e43ba8f307b67112a6f7e7929db3dcf
Palak Dhingra <palak24dhingra@gmail.com>
2022-01-11 11:18:57 +0530

    initial commit

---
+++ src/striverCode.js
@@ -0,0 +1,181 @@
+export const code = [
+  {
+    question: "Set Matrix Zeroes",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/set-matrix-zeroes/",
+    date: "Day 1 | Q1",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/set-matrix-zeroes/"
+  },
+  {
+    question: "Pascal's Triangle",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/pascals-triangle/",
+    date: "Day 1 | Q2",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/pascals-triangle/"
+  },
+  {
+    question: "Next Permutation",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/next-permutation/",
+    date: "Day 1 | Q3",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/next-permutation/"
+  },
+  {
+    question: "Maximum Subarray",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/maximum-subarray/",
+    date: "Day 1 | Q4",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/maximum-subarray/"
+  },
+  {
+    question: "Sort an array of 0's 1's & 2's",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/sort-colors/",
+    date: "Day 1 | Q5",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/sort-colors/"
+  },
+  {
+    question: "Best Time to Buy and Sell Stock",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/best-time-to-buy-and-sell-stock/",
+    date: "Day 1 | Q6",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/best-time-to-buy-and-sell-stock/"
+  },
+  {
+    question: "Rotate Image",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/rotate-image/",
+    date: "Day 2 | Q1",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/rotate-image/"
+  },
+  {
+    question: "Merge Intervals",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/merge-intervals/",
+    date: "Day 2 | Q2",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/merge-intervals/"
+  },
+  {
+    question: "Merge Sorted Array",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/merge-sorted-array/",
+    date: "Day 2 | Q3",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/merge-sorted-array/"
+  },
+  {
+    question: "Find the Duplicate Number",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/find-the-duplicate-number/",
+    date: "Day 2 | Q4",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/find-the-duplicate-number/"
+  },
+  {
+    question: "Repeat and Missing Number Array",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/repeat-and-missing-number-array/",
+    date: "Day 2 | Q5",
+    type: "arrays",
+    problem:
+      "https://www.interviewbit.com/problems/repeat-and-missing-number-array/"
+  },
+  {
+    question: "Count Inversions",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/counting-inversions/",
+    date: "Day 2 | Q6",
+    type: "arrays",
+    problem:
+      "https://www.codingninjas.com/codestudio/problems/count-inversions_615"
+  },
+  {
+    question: "Search a 2D Matrix",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/search-a-2d-matrix/",
+    date: "Day 3 | Q1",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/search-a-2d-matrix/"
+  },
+  {
+    question: "Pow(x, n)",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/powx-n/",
+    date: "Day 3 | Q2",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/powx-n/"
+  },
+  {
+    question: "Majority Element",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/majority-element/",
+    date: "Day 3 | Q3",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/majority-element/"
+  },
+  {
+    question: "Majority Element II",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/majority-element-ii/",
+    date: "Day 3 | Q4",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/majority-element-ii/"
+  },
+  {
+    question: "Unique Paths",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/unique-paths/",
+    date: "Day 3 | Q5",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/unique-paths/"
+  },
+  {
+    question: "Reverse Pairs",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/reverse-pairs/",
+    date: "Day 3 | Q6",
+    type: "arrays",
+    problem: "https://leetcode.com/problems/reverse-pairs/"
+  },
+  {
+    question: "Two Sum",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/two-sum/",
+    date: "Day 4 | Q1",
+    type: "hashing",
+    problem: "https://leetcode.com/problems/two-sum"
+  },
+  {
+    question: "4Sum",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/4sum/",
+    date: "Day 4 | Q2",
+    type: "hashing",
+    problem: "https://leetcode.com/problems/4sum/"
+  },
+  {
+    question: "Longest Consecutive Sequence",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/longest-consecutive-sequence/",
+    date: "Day 4 | Q3",
+    type: "hashing",
+    problem: "https://leetcode.com/problems/longest-consecutive-sequence/"
+  },
+  {
+    question: "Largest subarray with 0 sum",
+    solution:
+      "https://github.com/palak-dhingra/striver_sheet/blob/main/coding_solution/largest_subarray_with_0_sum.cpp",
+    date: "Day 4 | Q4",
+    type: "hashing",
+    problem:
+      "https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1"
+  }
+];
